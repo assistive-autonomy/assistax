7ce2b3cf426a86b55147b4863f50af0e1aa62c99
Leo Hinck <lh.from.kb@gmail.com>
2026-01-20 09:41:59 +0000

    sweeping configs

--- assistax/slurm_scripts/sweeps/ippo_ff_si_sweep.sh
+++ assistax/slurm_scripts/sweeps/ippo_ff_si_sweep.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=ippo_ff_sweep_si
 #
+#SBATCH --partition=PGR-Standard
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --time=5-00:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=16G
 #
 #SBATCH --gres=gpu:h200
+#
 #SBATCH --mail-user=l.hinckeldey@ed.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.90
 cd /home/s2618563/assistax 
 ulimit -n 10000
-uv run python assistax/baselines/ippo/ippo_sweep_old.py -cn ippo_sweep -m network=ff_nps ++num_seeds=6 ++env_name=scratchitch ++total_timesteps=4e7 ++batch_size=128,256,512 ++update_epochs=4,8,16 "++seed=range(0,5)" sweep.num_configs=8
+
+uv run python assistax/baselines/IPPO/ippo_sweep.py -cn ippo_sweep -m network=ff_nps ++NUM_SEEDS=6 ++ENV_NAME=scratchitch ++TOTAL_TIMESTEPS=4e7 ++NUM_MINIBATCHES=4,8,16 ++UPDATE_EPOCHS=4,8,16 "++SEED=range(0,2)" SWEEP.num_configs=8
--- assistax/slurm_scripts/sweeps/ippo_rnn_si_sweep.sh
+++ assistax/slurm_scripts/sweeps/ippo_rnn_si_sweep.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=ippo_rnn_sweep_si
 #
+#SBATCH --partition=PGR-Standard
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --time=5-00:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=16G
 #
 #SBATCH --gres=gpu:h200
+#
 #SBATCH --mail-user=l.hinckeldey@ed.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -14,4 +16,4 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.90
 cd /home/s2618563/assistax 
 ulimit -n 10000
 
-uv run python ippo_sweep_old.py -cn ippo_sweep -m network=rnn_nps ++NUM_SEEDS=6 ++ENV_NAME=scratchitch ++TOTAL_TIMESTEPS=4e7 ++BATCH_SIZE=128,256,512 ++UPDATE_EPOCHS=4,8,16 "++SEED=range(0,12)" SWEEP.num_configs=4
+uv run python assistax/baselines/IPPO/ippo_sweep.py -cn ippo_sweep -m network=rnn_nps ++NUM_SEEDS=6 ++ENV_NAME=scratchitch ++TOTAL_TIMESTEPS=4e7 ++NUM_MINIBATCHES=4,8,16 ++UPDATE_EPOCHS=4,8,16 "++SEED=range(0,6)" SWEEP.num_configs=3
