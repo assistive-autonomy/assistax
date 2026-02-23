802a244980042787a2cbc71a387275229f9f27ae
Leo <lh.from.kb@gmail.com>
2026-02-23 11:29:12 +0000

    wandb logging for zsc

--- assistax/baselines/ZSC/ppo_aht.py
+++ assistax/baselines/ZSC/ppo_aht.py
@@ -241,6 +241,7 @@ def main(config):
             obs=False,
             info=False,
             avail_actions=False,
+            env_metrics=config.get("SAVE_METRICS", False)
         )
 
         # ===== DUAL EVALUATION EXECUTION =====
