3485bf304577a23734a5e7e8257f4801ff80fbbf
Leo <lh.from.kb@gmail.com>
2026-02-26 22:06:59 +0000

    mappo teeth kube zoo gen edit

--- assistax/kube_scripts/pop_gen/mappo_ff_teeth_zoo.sh
+++ assistax/kube_scripts/pop_gen/mappo_ff_teeth_zoo.sh
@@ -49,7 +49,8 @@ uv run python assistax/baselines/MAPPO/mappo_zoo_gen.py \
     ++NUM_STEPS=128 \
     ++ENV_KWARGS.preference_rewards.reward_budget=2.09 \
     ++ENV_KWARGS.disability.joint_restriction_factor=0 \
-    ++ENV_KWARGS.disability.joint_strength=0.5  
+    ++ENV_KWARGS.disability.joint_strength=0.5 \
+    ++ENV_KWARGS.preference_rewards.reward_budget=2.09 
 
 
 
@@ -67,7 +68,8 @@ uv run python assistax/baselines/MAPPO/mappo_zoo_gen.py \
     ++NUM_STEPS=128 \
     ++ENV_KWARGS.preference_rewards.reward_budget=2.09 \
     ++ENV_KWARGS.disability.joint_restriction_factor=0.5 \
-    ++ENV_KWARGS.disability.joint_strength=1
+    ++ENV_KWARGS.disability.joint_strength=1 \
+    ++ENV_KWARGS.preference_rewards.reward_budget=2.09
 
 uv run python assistax/baselines/MAPPO/mappo_zoo_gen.py \
     -cn $CONFIG -m\
@@ -83,7 +85,8 @@ uv run python assistax/baselines/MAPPO/mappo_zoo_gen.py \
     ++NUM_STEPS=128 \
     ++ENV_KWARGS.preference_rewards.reward_budget=2.09 \
     ++ENV_KWARGS.disability.joint_restriction_factor=1 \
-    ++ENV_KWARGS.disability.joint_strength=0.5
+    ++ENV_KWARGS.disability.joint_strength=0.5 \
+    ++ENV_KWARGS.preference_rewards.reward_budget=2.09
 
 # --- Cleanup workspace (but NOT the zoo shard) ---
 rm -rf "$WORK_DIR"
